114647442b969953cabc4c323096da8ee4434534
Ted Surihu <torleys@yahoo.com>
2021-12-07 22:52:31 -0500

    cors and bodyParser added to server file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var path = require('path');
-
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = '3000';
 
 app.use(express.static(path.join(__dirname, "/client/main")));
+app.use(cors());
+app.use(bodyParser.json());
 
 
 app.listen(port);
