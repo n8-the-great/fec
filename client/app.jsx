f76e6b75e0fa91329508054a412c9fdc1fed673d
Nate Rana <n8.rana@gmail.com>
2022-01-03 17:27:52 -0600

    restarting some work

--- client/app.jsx
+++ client/app.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './reviews/index.jsx';
+import RelatedProducts from './components/RelatedProducts.jsx'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      id: 59555,
+      related: []
     }
   }
+
 
   render() {
      return (
       <div>
-        <Reviews /> 
+        <Reviews />
+        <RelatedProducts id={this.state.id}/>
       </div>
-       
+
      );
   }
 }
---
+++ client/components/RelatedProducts.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class RelatedProducts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      related: null
+    }
+    this.relatedProductsRequest = this.relatedProductsRequest.bind(this);
+  };
+
+
+  populateRelated = (data) => {
+    this.setState({
+      related: data
+    });
+
+  }
+
+
+  relatedProductsRequest(id) {
+    var getRelatedFromURL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}/related`;
+
+    $.ajax({
+      url: getRelatedFromURL,
+      type: 'GET',
+      data: JSON.stringify(dataObj),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function (response) {
+        console.log('success!  see response');
+        console.log(response);
+        // populateRelated(response);
+        return response;
+      },
+      failure: function (response) {
+        console.log('failed!  see response');
+        console.log(response);
+        return response;
+      }
+    });
+  }
+
+  componentDidMount() {
+    console.log('component did mount');
+    console.log(typeof this.relatedProductsRequest(this.props.id));
+    // console.log(this.props.id);
+  }
+
+
+  render() {
+    console.log('hello');
+    console.log(this.props.id);
+    console.log(this.state);
+
+    return(
+      <div className="relatedProducts">
+        Related Products
+      </div>
+    );
+  }
+}
+
+export default RelatedProducts;
