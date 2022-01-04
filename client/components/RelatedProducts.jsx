c52c2bf54f930114cdfbf321a9b260fd66ae0cd6
Nate Rana <n8.rana@gmail.com>
2022-01-03 18:07:45 -0600

    ajax request added to pull related ids

--- client/components/RelatedProducts.jsx
+++ client/components/RelatedProducts.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import token from '../../config.js';
 
 class RelatedProducts extends React.Component {
   constructor(props) {
@@ -23,10 +24,15 @@ class RelatedProducts extends React.Component {
   relatedProductsRequest(id) {
     var getRelatedFromURL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}/related`;
 
+
+
     $.ajax({
       url: getRelatedFromURL,
       type: 'GET',
-      data: JSON.stringify(dataObj),
+      // data: JSON.stringify(dataObj),
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', token.ID);
+      },
       contentType: 'application/json',
       dataType: 'json',
       success: function (response) {
