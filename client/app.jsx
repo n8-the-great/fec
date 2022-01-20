8ef7976c11a80b247339637bae79c11ea398efd9
Ted Surihu <torleys@yahoo.com>
2022-01-20 15:50:29 -0500

    click tracking implemented with axios call added rather than just a console log

--- client/app.jsx
+++ client/app.jsx
@@ -87,6 +87,14 @@ getDateTime() {
         time: this.getDateTime()
       }
     }
+
+    axios(options)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 
 
