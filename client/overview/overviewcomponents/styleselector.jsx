91a5d48dfce61f871822bddc46a53d47839775a3
Wilson Lessley <wilsonlessley14@gmail.com>
2022-01-21 19:22:05 -0800

    upstream fetch complete

--- client/overview/overviewcomponents/styleselector.jsx
+++ client/overview/overviewcomponents/styleselector.jsx
@@ -8,24 +8,32 @@ const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
 class StyleSelector extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedStyle: 0
+    }
   }
 
   styleButtonClick(e) {
     e.preventDefault();
-    var styleClicked = this.findStyle(e.target.innerHTML);
-    console.log(styleClicked);
-    this.props.updateGallery(styleClicked);
+    var styleClicked = this.findStyle(e.target.alt);
+    this.props.updateGallery(styleClicked.returnStyle);
+    this.setState({
+      selectedStyle: styleClicked.styleIndex
+    });
   }
 
   findStyle(name) {
     var returnStyle = 'error';
+    var i = 0;
+    var styleIndex;
     this.props.styles.forEach(style => {
-      console.log(style.name === name);
       if (style.name === name) {
         returnStyle = style;
+        styleIndex = i;
       }
+      i++;
     });
-    return returnStyle;
+    return {returnStyle, styleIndex};
   }
 
   render() {
@@ -34,19 +42,38 @@ class StyleSelector extends React.Component {
       stylesArray[i] = this.props.styles.slice(i * 4, (i+1) * 4);
     }
 
+    var createStyleTD = (style) => {
+      var isSelected = false;
+      if (this.props.styles[this.state.selectedStyle].name === style.name) {
+        console.log(style.name);
+        isSelected = true;
+      }
+
+      //console.log(style.photos[0].thumbnail_url);
+      return <td className="table-data overview" onClick={this.styleButtonClick.bind(this)}>
+        <div className="mask overview">
+          <img className={(isSelected ? "selected-style" : "non-selected-style") + " style-button overview"} alt={style.name} src={style.photos[0].thumbnail_url}></img>
+        </div>
+      </td>
+    }
+
+
+    var styleTable = [];
+    styleTable = <table>
+      <tbody>{
+        stylesArray.map(arrayOf4Styles => (
+          <tr>{
+            arrayOf4Styles.map(style => {
+              return createStyleTD(style)
+            })
+          }</tr>
+        ))
+      }</tbody>
+    </table>
+
     return (
-      <div className="style-selector">
-        <table>
-          <tbody>{
-              stylesArray.map(arrayOf4Styles => (
-                <tr>{
-                  arrayOf4Styles.map(style => (
-                    <td onClick={this.styleButtonClick.bind(this)} className="style-button">{style.name}</td>
-                  ))
-                }</tr>
-              ))
-            }</tbody>
-        </table>
+      <div className="style-selector overview">
+        {styleTable}
 
       </div>
     )
